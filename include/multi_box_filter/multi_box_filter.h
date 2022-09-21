98c9611ed3f3cadaac0961faec43099ad95f1a34
woawo1213 <woawo1213@gmail.com>
2022-09-21 12:20:55 +0900

    get row dynamically

--- include/multi_box_filter/multi_box_filter.h
+++ include/multi_box_filter/multi_box_filter.h
@@ -1,37 +1,39 @@
 /*
  *  multi_box_filter.h
+ * 
  *
  *  author: Sebastian Pütz <spuetz@uni-osnabrueck.de>
- *  modified: jmSHIN <woawo1213@gmail.com>
+ *  modified: jm <jmshin@wonik.com>
  */
 
 #ifndef MULTIBOXFILTER_H
 #define MULTIBOXFILTER_H
 
-#include <boost/algorithm/string/replace.hpp>
-#include <boost/property_tree/json_parser.hpp>
-#include <boost/property_tree/ptree.hpp>
-#include <filters/filter_base.h>
+#include <string>
+#include <vector>
 #include <fstream>
-#include <geometry_msgs/Polygon.h>
 #include <iostream>
-#include <laser_geometry/laser_geometry.h>
 #include <ros/ros.h>
+#include <filters/filter_base.h>
 #include <sensor_msgs/LaserScan.h>
 #include <sensor_msgs/point_cloud_conversion.h>
-#include <string>
+#include <laser_geometry/laser_geometry.h>
+#include <geometry_msgs/Polygon.h>
 #include <tf/transform_datatypes.h>
 #include <tf/transform_listener.h>
-#include <vector>
 #include <visualization_msgs/Marker.h>
 #include <visualization_msgs/MarkerArray.h>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/algorithm/string/replace.hpp>
 
-namespace laser_filters {
+namespace laser_filters
+{
 /**
- * @brief This is a filter that removes points in a laser scan inside of a
- * cartesian box.
+ * @brief This is a filter that removes points in a laser scan inside of a cartesian box.
  */
-class Box {
+class Box
+{
 public:
     double x_min_, x_max_, y_min_, y_max_;
 
@@ -47,143 +49,185 @@ public:
     }
 };
 
-class ParameterPaser {
+
+class ParameterPaser
+{
     ros::NodeHandle nh_;
-    int row = 4, colum = 4, cell = 2;
+    int colum = 4, cell = 2;
     float matrix[4][4][2];
     boost::property_tree::ptree pt;
-
 public:
     ParameterPaser() {}
 
-    void read_param(XmlRpc::XmlRpcValue &box_xmlrpc) {
+    void read_param(XmlRpc::XmlRpcValue& box_xmlrpc)
+    {
+        int row;
         std::string xml_str = box_xmlrpc;
         boost::property_tree::ptree iroot;
 
-        boost::replace_all(xml_str, "(", "[");
-        boost::replace_all(xml_str, ")", "]");
-        xml_str = "{\"target_model\":" + xml_str + "}";
+        row = count_row(xml_str);
+
+        boost::replace_all(xml_str,"(","[");
+        boost::replace_all(xml_str,")","]");
+        xml_str="{\"target_model\":" + xml_str + "}";
 
         std::stringstream ss(xml_str);
         boost::property_tree::read_json(ss, iroot);
 
         int x_ = 0;
-        for (auto &row : iroot.get_child("target_model")) {
-        int y_ = 0;
-        for (auto &colum : row.second) {
-            int z_ = 0;
-            for (auto &cell : colum.second) {
-            matrix[x_][y_][z_] = cell.second.get_value<float>();
-            z_++;
+        for (auto &row : iroot.get_child("target_model"))
+        {
+            int y_ = 0;
+            for (auto &colum : row.second)
+            {
+                int z_ = 0;
+                for(auto &cell : colum.second)
+                {
+                    matrix[x_][y_][z_] = cell.second.get_value<float>();
+                    z_++;
+                }
+                y_++;
             }
-            y_++;
-        }
-        x_++;
+            x_++;
         }
     }
 
-    std::vector<Box> generate_box() {
+    std::vector<Box> generate_box(XmlRpc::XmlRpcValue& box_xmlrpc)
+    {
         Box box;
         std::vector<Box> genbox;
+        std::string xml_str = box_xmlrpc;
 
+        int row;
         float margin_x = 0.1;
         float margin_y = 0.1;
         float center_offset = 0.341;
-        for (int i = 0; i < row; i++) {
-        for (int j = 0; j < colum; j += 3) {
-            for (int k = 0; k < cell; k++) {
-            if (j == 0) {
-                if (k == 0)
-                box.x_max_ = matrix[i][j][k] + margin_x - center_offset;
-                else
-                box.y_max_ = matrix[i][j][k] + margin_y;
-            } else {
-                if (k == 0)
-                box.x_min_ = matrix[i][j][k] - margin_x - center_offset;
-                else
-                box.y_min_ = matrix[i][j][k] - margin_y;
+
+        row = count_row(xml_str);
+
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < colum; j+=3)
+            {
+                for(int k = 0; k < cell; k++)
+                {
+                    if(j == 0)
+                    {
+                        if(k == 0)
+                            box.x_max_ = matrix[i][j][k] + margin_x - center_offset;
+                        else
+                            box.y_max_ = matrix[i][j][k] + margin_y ;
+                    }
+                    else
+                    {
+                        if(k == 0)
+                            box.x_min_ = matrix[i][j][k] - margin_x - center_offset;
+                        else
+                            box.y_min_ = matrix[i][j][k] - margin_y ;
+                    }
+                }
             }
-            }
+            genbox.push_back(box);
         }
-        genbox.push_back(box);
-        }
-        return genbox;
+        return genbox;       
     }
+
+    int count_row(std::string str)
+    {
+        int row;
+        row = (count(str.begin(), str.end(), ',') / 8) + 1;
+        
+        return row; 
+    }
+    
+
 };
 
-class MarkerPublisher {
+
+class MarkerPublisher
+{
     ros::Publisher pub_marker_;
     visualization_msgs::MarkerArray ma;
 
 public:
-    MarkerPublisher() {
+    MarkerPublisher() 
+    {   
+        std::string target_model;
         ros::NodeHandle nh_("~");
-        pub_marker_ =
-            nh_.advertise<visualization_msgs::MarkerArray>("multi_box_marker", 1);
+        pub_marker_=nh_.advertise<visualization_msgs::MarkerArray>("multi_box_marker",1);
     }
 
-    void initialize(std::vector<Box> &box) {
+
+    void initialize(std::vector<Box>& box) {
 
         geometry_msgs::Point box_points;
 
-        for (int i = 0; i < box.size(); i++) {
-        visualization_msgs::Marker marker_;
-        marker_.header.frame_id = "base_link";
-        marker_.header.stamp = ros::Time::now();
-        marker_.ns = "multi_box_marker";
-        marker_.id = i;
-        marker_.type = visualization_msgs::Marker::LINE_STRIP;
-        marker_.action = visualization_msgs::Marker::ADD;
-        marker_.pose.orientation.w = 1.0;
-        marker_.scale.x = 0.008;
-        marker_.color.a = 1.0;
-        marker_.color.b = 1.0;
-        marker_.lifetime = ros::Duration();
+        for(int i = 0; i < box.size(); i++)
+        {
+            visualization_msgs::Marker marker_;
+            marker_.header.frame_id = "base_link";
+            marker_.header.stamp = ros::Time::now();
+            marker_.ns = "multi_box_marker";
+            marker_.id = i;
+            marker_.type = visualization_msgs::Marker::LINE_STRIP;
+            marker_.action = visualization_msgs::Marker::ADD;
+            marker_.pose.orientation.w = 1.0;
+            marker_.scale.x = 0.008;
+            marker_.color.a = 1.0;
+            marker_.color.b = 1.0;
+            marker_.lifetime=ros::Duration();
 
-        // top left
-        box_points.x = box[i].x_max_;
-        box_points.y = box[i].y_max_;
-        marker_.points.push_back(box_points);
+            //top left
+            box_points.x = box[i].x_max_;
+            box_points.y = box[i].y_max_;
+            marker_.points.push_back(box_points);
 
-        // top right
-        box_points.x = box[i].x_max_;
-        box_points.y = box[i].y_min_;
-        marker_.points.push_back(box_points);
+            //top right
+            box_points.x = box[i].x_max_;
+            box_points.y = box[i].y_min_;
+            marker_.points.push_back(box_points);
 
-        // bottom right
-        box_points.x = box[i].x_min_;
-        box_points.y = box[i].y_min_;
-        marker_.points.push_back(box_points);
+            //bottom right
+            box_points.x = box[i].x_min_;
+            box_points.y = box[i].y_min_;
+            marker_.points.push_back(box_points);
 
-        // bottom right
-        box_points.x = box[i].x_min_;
-        box_points.y = box[i].y_max_;
-        marker_.points.push_back(box_points);
+            //bottom right
+            box_points.x = box[i].x_min_;
+            box_points.y = box[i].y_max_;
+            marker_.points.push_back(box_points);
 
-        // top left
-        box_points.x = box[i].x_max_;
-        box_points.y = box[i].y_max_;
-        marker_.points.push_back(box_points);
+            //top left
+            box_points.x = box[i].x_max_;
+            box_points.y = box[i].y_max_;
+            marker_.points.push_back(box_points);
 
-        ma.markers.push_back(marker_);
+            ma.markers.push_back(marker_);
         }
+
     }
 
-    void publish(ros::Time stamp) {
-        for (int i = 0; i < ma.markers.size(); i++) {
-        ma.markers[i].header.stamp = stamp;
+    void publish(ros::Time stamp) 
+    {
+        for(int i=0;i<ma.markers.size();i++)
+        {
+            ma.markers[i].header.stamp=stamp;
         }
         pub_marker_.publish(ma);
     }
+
 };
 
-class LaserScanMultiBoxFilter : public filters::FilterBase<sensor_msgs::LaserScan> {
+
+
+class LaserScanMultiBoxFilter : public filters::FilterBase<sensor_msgs::LaserScan>
+{
     std::string box_frame_;
     std::string box_param_;
     std::vector<Box> box_;
 
     laser_geometry::LaserProjection projector_;
-
+    
     // tf listener to transform scans into the box_frame
     tf::TransformListener tf_;
     bool invert_filter;
@@ -193,12 +237,15 @@ class LaserScanMultiBoxFilter : public filters::FilterBase<sensor_msgs::LaserSca
     MarkerPublisher marker_pub_;
 
 public:
+    
     LaserScanMultiBoxFilter() {}
     bool configure();
-    bool update(const sensor_msgs::LaserScan &input_scan,
-                sensor_msgs::LaserScan &filtered_scan);
+    bool update(
+        const sensor_msgs::LaserScan& input_scan,
+        sensor_msgs::LaserScan& filtered_scan);
+
 };
 
-} // namespace laser_filters
+}
 
 #endif /* multi_box_filter.h */
--- src/multi_box_filter.cpp
+++ src/multi_box_filter.cpp
@@ -4,91 +4,102 @@
  *  multi_box_filter.cpp
  *
  *  author: Sebastian Pütz <spuetz@uni-osnabrueck.de>
- *  modified: jmSHIN <woawo1213@gmail.com>
+ *  modified: jm <jmshin@wonik.com>
  */
 #include <string>
 #include <vector>
 
+#include <ros/ros.h>
+#include "sensor_msgs/LaserScan.h"
 #include "filters/filter_base.h"
 #include "multi_box_filter/multi_box_filter.h"
-#include "sensor_msgs/LaserScan.h"
-#include <ros/ros.h>
 
 #include "pluginlib/class_list_macros.h"
 
-PLUGINLIB_EXPORT_CLASS(laser_filters::LaserScanMultiBoxFilter,
-                       filters::FilterBase<sensor_msgs::LaserScan>)
+PLUGINLIB_EXPORT_CLASS(laser_filters::LaserScanMultiBoxFilter, filters::FilterBase<sensor_msgs::LaserScan>)
 
-bool laser_filters::LaserScanMultiBoxFilter::configure() {
+
+bool laser_filters::LaserScanMultiBoxFilter::configure()
+{   
     XmlRpc::XmlRpcValue box_xmlrpc_;
     up_and_running_ = true;
 
-    bool box_set = getParam("box", box_xmlrpc_);
+    bool box_set = getParam("box",box_xmlrpc_);
     bool box_frame_set = getParam("box_frame", box_frame_);
-    bool invert_set = getParam("invert", invert_filter);
+    bool invert_set    = getParam("invert", invert_filter);
 
     parser_.read_param(box_xmlrpc_);
-    box_ = parser_.generate_box();
+    box_ = parser_.generate_box(box_xmlrpc_);
     marker_pub_.initialize(box_);
 
     ROS_INFO("Multi Box filter started!");
 
-    if (!box_frame_set) {
+    if(!box_frame_set)
+    {
         ROS_ERROR("box_frame is not set!");
     }
-    if (!box_set) {
+    if(!box_set)
+    {
         ROS_ERROR("box is not set!");
     }
-    if (!invert_set) {
+    if(!invert_set)
+    {
         ROS_INFO("invert filter not set, assuming false");
-        invert_filter = false;
+        invert_filter=false;
     }
 
     return box_frame_set && box_set;
 }
 
 bool laser_filters::LaserScanMultiBoxFilter::update(
-    const sensor_msgs::LaserScan &input_scan,
-    sensor_msgs::LaserScan &output_scan) {
+    const sensor_msgs::LaserScan& input_scan,
+    sensor_msgs::LaserScan &output_scan)
+{
     output_scan = input_scan;
-
+    
     sensor_msgs::PointCloud2 laser_cloud;
-
+    
     std::string error_msg;
 
     bool success = tf_.waitForTransform(
-        box_frame_, input_scan.header.frame_id,
-        input_scan.header.stamp +
-            ros::Duration().fromSec(input_scan.ranges.size() *
-                                    input_scan.time_increment),
-        ros::Duration(1.0), ros::Duration(0.01), &error_msg);
-    if (!success) {
-        ROS_WARN("Could not get transform, irgnoring laser scan! %s",
-                error_msg.c_str());
+        box_frame_,
+        input_scan.header.frame_id,
+        input_scan.header.stamp + ros::Duration().fromSec(input_scan.ranges.size()*input_scan.time_increment),
+        ros::Duration(1.0),
+        ros::Duration(0.01),
+        &error_msg
+    );
+    if(!success)
+    {
+        ROS_WARN("Could not get transform, irgnoring laser scan! %s", error_msg.c_str());
         return false;
     }
-    // polar to cartesian
-    try {
-        projector_.transformLaserScanToPointCloud(box_frame_, input_scan,
-                                                laser_cloud, tf_);
-    } catch (tf::TransformException &ex) {
-        if (up_and_running_) {
-        ROS_WARN_THROTTLE(1, "Dropping Scan: Tansform unavailable %s", ex.what());
-        return true;
-        } else {
-        ROS_INFO_THROTTLE(.3, "Ignoring Scan: Waiting for TF");
+    //polar to cartesian
+    try
+    {
+        projector_.transformLaserScanToPointCloud(box_frame_, input_scan, laser_cloud, tf_);
+    }
+    catch(tf::TransformException& ex)
+    {
+        if(up_and_running_)
+        {
+            ROS_WARN_THROTTLE(1, "Dropping Scan: Tansform unavailable %s", ex.what());
+            return true;
+        }
+        else
+        {
+            ROS_INFO_THROTTLE(.3, "Ignoring Scan: Waiting for TF");
         }
         return false;
     }
-    const int i_idx_c =
-        sensor_msgs::getPointCloud2FieldIndex(laser_cloud, "index");
+    const int i_idx_c = sensor_msgs::getPointCloud2FieldIndex(laser_cloud, "index");
     const int x_idx_c = sensor_msgs::getPointCloud2FieldIndex(laser_cloud, "x");
     const int y_idx_c = sensor_msgs::getPointCloud2FieldIndex(laser_cloud, "y");
     const int z_idx_c = sensor_msgs::getPointCloud2FieldIndex(laser_cloud, "z");
 
-    if (i_idx_c == -1 || x_idx_c == -1 || y_idx_c == -1 || z_idx_c == -1) {
-        ROS_INFO_THROTTLE(.3,
-                        "x, y, z and index fields are required, skipping scan");
+    if(i_idx_c == -1 || x_idx_c == -1 || y_idx_c == -1 || z_idx_c == -1)
+    {
+        ROS_INFO_THROTTLE(.3, "x, y, z and index fields are required, skipping scan");
     }
 
     const int i_idx_offset = laser_cloud.fields[i_idx_c].offset;
@@ -100,31 +111,37 @@ bool laser_filters::LaserScanMultiBoxFilter::update(
     const long int pcount = laser_cloud.width * laser_cloud.height;
     const long int limit = pstep * pcount;
 
-    int i_idx, x_idx, y_idx, z_idx;
+    int i_idx, x_idx, y_idx, z_idx;  
 
-    for (i_idx = i_idx_offset, x_idx = x_idx_offset, y_idx = y_idx_offset,
+    for(
+        i_idx = i_idx_offset,
+        x_idx = x_idx_offset,
+        y_idx = y_idx_offset,
         z_idx = z_idx_offset;
 
         x_idx < limit;
 
-        i_idx += pstep, x_idx += pstep, y_idx += pstep, z_idx += pstep) {
-        // TODO works only for float data types and with an index field
-        // I'm working on it, see https://github.com/ros/common_msgs/pull/78
-        float x = *((float *)(&laser_cloud.data[x_idx]));
-        float y = *((float *)(&laser_cloud.data[y_idx]));
-        float z = *((float *)(&laser_cloud.data[z_idx]));
-        int index = *((int *)(&laser_cloud.data[i_idx]));
+        i_idx += pstep,
+        x_idx += pstep,
+        y_idx += pstep,
+        z_idx += pstep)
+    {
+    // TODO works only for float data types and with an index field
+    // I'm working on it, see https://github.com/ros/common_msgs/pull/78 
+        float x = *((float*)(&laser_cloud.data[x_idx]));
+        float y = *((float*)(&laser_cloud.data[y_idx]));
+        float z = *((float*)(&laser_cloud.data[z_idx]));
+        int index = *((int*)(&laser_cloud.data[i_idx]));
 
         // invert_filter 신경 안씀
 
-        for (int i = 0; i < box_.size(); i++) {
-        if (box_[i].is_in(x, y)) {
-            // 2022.01.27 : jglee
-            // obstacle_layer 에서 cart_leg 를 지워줘야 하기 때문에, inf data 로
-            // clear 함(Nan -> inf).
-            output_scan.ranges[index] = std::numeric_limits<float>::infinity();
-            break;
-        }
+        for(int i = 0; i < box_.size(); i++) {
+            if(box_[i].is_in(x, y)) {
+                // 2022.01.27 : jglee
+                // obstacle_layer 에서 cart_leg 를 지워줘야 하기 때문에, inf data 로 clear 함(Nan -> inf).
+                output_scan.ranges[index] = std::numeric_limits<float>::infinity();
+                break;
+            }
         }
     }
     marker_pub_.publish(input_scan.header.stamp);
